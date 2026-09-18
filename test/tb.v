3d51eae46d2a39fb0d333cee456e5fd6e5006d71
divadnauj-GB <juan.david.guerrero.balaguera@gmail.com>
2026-09-18 00:16:10 -0500

    added makefile update for ihp

--- test/tb.v
+++ test/tb.v
@@ -32,8 +32,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+  //wire VPWR = 1'b1;
+  //wire VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
@@ -41,8 +41,8 @@ module tb ();
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      //.VPWR(VPWR),
+      //.VGND(VGND),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
